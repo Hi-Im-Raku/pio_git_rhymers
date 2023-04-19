8f9f77e551e169ebbaa049948f3db48e032325dc
Weronika Kretowicz <235903@edu.p.lodz.pl>
2023-04-19 12:50:13 +0200

    Poprawa stałe_i_annotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int INT = -1;
-    public static final int INT1 = 12;
-    private int[] numbers = new int[INT1];
+    public static int EMPTY = -1;
+    public static int IF_EMPTY = -1;
 
-    public int total = INT;
+    public static int MAX_SIZE = 12;
+    private final int[] numbers = new int[MAX_SIZE];
+
+    public int total = EMPTY;
 
 
     public void countIn(int in) {
@@ -15,22 +17,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == INT;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == (MAX_SIZE - 1);
     }
 
     protected int peekaBoo() {
         if (callCheck())
-            return INT;
+            return IF_EMPTY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return INT;
+            return IF_EMPTY;
         return numbers[total--];
     }
 
